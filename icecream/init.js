d68561d7bb1ff648e775bcb5cbdf6059d7cba58f
Lee min young <lmy1147@naver.com>
2025-02-12 15:48:50 +0900

    0212

---
+++ icecream/init.js
@@ -0,0 +1,8 @@
+const productData = [
+  {prodCode:'P001', prodName: 'Strawberry Ice Cream', price: 3500, image:'images/strawberry-ice-cream.png'},
+  {prodCode:'P002', prodName: 'Chocolate Ice Cream', price: 4000, image:'images/chocolate-ice-cream.png'},
+  {prodCode:'P003', prodName: 'Coffee Ice Cream', price: 3200, image:'images/coffee-ice-creampng.png'},
+  {prodCode:'P004', prodName: 'Pistachio Ice Cream', price: 4200, image:'images/pistachio-ice-cream.png'},
+  {prodCode:'P005', prodName: 'Blueberry Ice Cream', price: 3200, image:'images/blueberry.png'},
+  {prodCode:'P006', prodName: 'Mint chocolate Ice Cream', price: 4200, image:'images/mint-chocolate.png'},
+]
---
+++ icecream/product2.js
@@ -0,0 +1,16 @@
+let productData = JSON.parse(localStorage.getItem('productData'));
+console.log(productData);
+
+productData.forEach(function(product){
+  let str=`
+  <div class="col-md-4">
+  <div class="cream_box">
+     <div class="cream_img"><img src="${product.image}"></div>
+     <div class="price_text">${product.price}won</div>
+     <h6 class="strawberry_text">${product.prodName}</h6>
+     <div class="cart_bt"><a href="#">Add To Cart</a></div>
+  </div>`;
+
+  let target = document.querySelector('div.cream_section_2');
+  target.insertAdjacentHTML('beforeend', str);
+});
